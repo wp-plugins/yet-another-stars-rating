4bdec76f09fdd4a209fe60e3630fa5b6f9a12613
Dudo <Dudo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-22 14:29:05 +0000

    dir content 0.6.3
    git-svn-id: https://plugins.svn.wordpress.org/yet-another-stars-rating/tags/0.6.3@1012171 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ yasr-settings-page.php
@@ -0,0 +1,325 @@
+<?php 
+
+/*
+
+Copyright 2014 Dario Curvino (email : d.curvino@tiscali.it)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit('You\'re not allowed to see this page'); // Exit if accessed directly
+
+if ( !current_user_can( 'manage_options' ) ) {
+	wp_die( __( 'You do not have sufficient permissions to access this page.', 'yasr' ));
+}
+
+$n_multi_set = NULL;
+
+?>
+
+	<div class="wrap">
+
+        <h2>Yet Another Stars Rating: <?php _e("Settings", "yasr"); ?></h2>
+
+        <?php
+
+        $error_new_multi_set=yasr_process_new_multi_set_form(); //defined in yasr-settings-functions
+
+        $error_edit_multi_set=yasr_process_edit_multi_set_form(); //defined in yasr-settings-functions
+
+        if ($error_new_multi_set) {
+        	echo "<div class=\"error\"> <p> <strong>";
+
+      		foreach ($error_new_multi_set as $error) {
+      			_e($error, 'yasr'); 
+      			echo "<br />";
+      		}
+
+    		echo "</strong></p></div>"; 
+    	}
+
+        if ($error_edit_multi_set) {
+        	echo "<div class=\"error\"> <p> <strong>";
+
+      		foreach ($error_edit_multi_set as $error) {
+      			_e($error, 'yasr'); 
+      			echo "<br />";
+      		}
+
+    		echo "</strong></p></div>"; 
+    	}
+
+			
+		if (isset($_GET['tab'])) {
+    		$active_tab = $_GET[ 'tab' ];
+		}
+
+		else {
+			$active_tab = 'general_settings';
+		}
+
+
+		?>
+
+        <h2 class="nav-tab-wrapper yasr-no-underline">
+            <a href="?page=yasr_settings_page&tab=general_settings" class="nav-tab <?php if ($active_tab == 'general_settings' || ($active_tab != 'manage_multi' && $active_tab != 'style_options' && $active_tab != 'go_pro')) echo 'nav-tab-active'; ?>" > <?php _e("General Settings", "yasr"); ?> </a>
+            <a href="?page=yasr_settings_page&tab=manage_multi" class="nav-tab <?php if ($active_tab == 'manage_multi') echo 'nav-tab-active'; ?>" > <?php _e("Multi Sets", "yasr"); ?> </a>
+            <a href="?page=yasr_settings_page&tab=style_options" class="nav-tab <?php if ($active_tab == 'style_options') echo 'nav-tab-active'; ?>" > <?php _e("Styles", "yasr"); ?> </a>
+            <a href="?page=yasr_settings_page&tab=go_pro" class="nav-tab <?php if ($active_tab == 'go_pro') echo 'nav-tab-active'; ?>" > <?php _e("Pro Features!", "yasr"); ?> </a>
+
+        </h2>
+
+
+
+        <?php 
+
+        if ($active_tab == 'general_settings' || $active_tab != 'manage_multi' && $active_tab != 'style_options' && $active_tab != 'go_pro') {
+
+        	?>
+
+		    <div class="yasr-settingsdiv">
+		        <form action="options.php" method="post" id="yasr_settings_form">
+		            <?php
+			            settings_fields( 'yasr_general_options_group' );
+			            do_settings_sections('yasr_general_settings_tab' );
+		            	submit_button( __('Save') );
+		           	?>
+		       	</form>
+		    </div>
+
+		    <?php yasr_donate_dx(); ?>
+
+			<div class="yasr-space-settings-div">
+			</div>
+
+			<?php 
+
+			$gd_star_rating_found = yasr_search_gd_star_rating();
+
+			$gd_star_imported = get_option('yasr-gdstar-imported');
+
+			//If gdstar rating has been found but data haven't been imported yet
+			if ($gd_star_rating_found && !$gd_star_imported) {
+				?>
+				<div class="yasr-settingsdiv">
+					<h3><?php _e("Import Gd Star Rating", "yasr"); ?></h3>
+					<?php _e("I've found a previous installation of Gd Star Rating.", "yasr"); ?> <br /><?php _e("Do you want proceed to import data?", "yasr"); ?>
+					<br />
+		        	<button href="#" class="button-delete" id="import-gdstar"><?php _e('Yes, Begin Import', 'yasr'); ?></button>
+
+		        	<div id="yasr-import-gdstar-div" style="display:none;">
+		          			<strong>
+		          				<?php _e("Click on Proceed to import Gd Star Rating data."); ?>
+		          			</strong>
+		          				<br />
+		          				<button href="#" class="button-primary" id="import-button"> <?php _e('Proceed', 'yasr'); ?></button>
+
+		          				<span id="yasr-loader-importer" style="display:none;" >&nbsp;<img src="<?php echo YASR_IMG_DIR . "loader.gif" ?>">
+		          				</span>
+		          				<br />
+
+		          			<div id="result-import">	
+		          			</div>
+					</div>
+				</div>
+
+				<div class="yasr-space-settings-div">
+				</div>
+
+			<?php
+
+			} //End If $gd_star_rating_found && !$gd_star_imported
+
+			else if ($gd_star_rating_found && $gd_star_imported==1) {
+				?>
+
+				<div class="yasr-settingsdiv">
+					<h3><?php _e("Manage GD Star Data", "yasr"); ?></h3>
+					<?php _e("Gd Star Rating has been already imported."); ?> <br />
+					<?php _e("If you wish you can import it again, but", 'yasr'); ?><strong> <?php _e("you will lose all data you've collect since the import!", "yasr"); ?> </strong> 
+					<br />
+		        	<button href="#" class="button-delete" id="import-gdstar"><?php _e('Ok, Import Again'); ?></button>
+
+		        	<div id="yasr-import-gdstar-div" style="display:none;">
+	          			<strong>
+	          				<?php _e("Click on Proceed to import again Gd Star Rating data. This may take a while!"); ?>
+	          			</strong>
+	          				<br />
+	          				<button href="#" class="button-primary" id="import-button"> <?php _e('Proceed', 'yasr'); ?></button>
+
+	          				<span id="yasr-loader-importer" style="display:none;" >&nbsp;<img src="<?php echo YASR_IMG_DIR . "loader.gif" ?>">
+	          				</span>
+	          				
+	          				<br />
+
+	          			<div id="result-import">	
+	          			</div>
+
+					</div>
+				</div>
+
+				<div class="yasr-space-settings-div">
+				</div>
+
+				<?php
+			} //$gd_star_rating_found && $gd_star_imported==1$gd_star_rating_found = yasr_search_gd_star_rating();
+
+		} //End if tab 'general_settings'
+
+		?>
+
+
+		<?php 
+
+		if ($active_tab == 'manage_multi') {
+
+			$multi_set=yasr_get_multi_set();
+
+			global $wpdb;
+
+			$n_multi_set = $wpdb->num_rows; //wpdb->num_rows always store the last of the last query
+
+			?>
+
+			<div class="yasr-settingsdiv">
+					
+				<h3> <?php _e("Manage Multi Set", "yasr"); ?></h3>
+
+				<p>
+
+					<a href="#" id="yasr-multi-set-doc-link"><?php _e("What is a Multi Set?", "yasr") ?></a>
+
+				</p>
+
+				<div id="yasr-multi-set-doc-box" style="display:none">
+					<?php _e("Multi Set allows you to insert a rate for each aspect about the product / local business / whetever you're reviewing, example in the image below.", "yasr");
+
+					echo "<br /><br /><img src=" . YASR_IMG_DIR . "/yasr-multi-set.png> <br /> <br />";
+
+					_e("You can create up to 99 different Multi Set and each one can contain up to 9 different fields. Once you've saved it, you can insert the rates while typing your article in the box below the editor, as you can see in this image (click to see it larger)", "yasr");
+
+					echo "<br /><br /><a href=\"" . YASR_IMG_DIR ."yasr-multi-set-insert-rate.jpg\"><img src=" . YASR_IMG_DIR . "/yasr-multi-set-insert-rate-small.jpg></a> <br /> <br />";
+
+					_e("In order to insert your Multi Sets into a post or page, you can either past the short code that will appear at the bottom of the box or just click on the star in the graphic editor and select \"Insert Multi Set\".", "yasr");
+
+					?>
+
+					<br /> <br />
+
+					<a href="#" id="yasr-multi-set-doc-link-hide"><?php _e("Close this message", "yasr") ?></a>
+
+				</div>
+
+				<div class="yasr-multi-set-left">
+
+					<div class="yasr-new-multi-set" >
+
+						<?php yasr_display_multi_set_form(); ?>
+
+					</div> <!--yasr-new-multi-set-->
+
+
+				</div> <!--End yasr-multi-set-left-->
+
+				<div class="yasr-multi-set-right">
+
+					<?php yasr_edit_multi_form(); ?>
+
+					<div id="yasr-multi-set-response" style="display:none">
+
+					</div>
+
+				</div> <!--End yasr-multi-set-right-->
+
+
+			</div>
+
+			<div class="yasr-donatedivdx" style="display:none">
+		        <h3><?php _e('Donations', 'yasr'); ?></h3>
+
+		        	<?php _e('If you have found this plugin useful, please consider making a donation to help support future development. Your support will be much appreciated. ', 'yasr'); ?>
+		        	<br />
+		        	<?php _e('Thank you!', 'yasr'); ?>
+		        	<br />
+		        	
+		        	<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=AXE284FYMNWDC">
+		        		<?php echo("<img src=" . YASR_IMG_DIR . "/paypal.png>"); ?>
+		        	</a>
+
+	        </div>
+
+			<div class="yasr-space-settings-div">
+			</div>
+
+			<?php
+
+		} //End if ($active_tab=='manage_multi')
+
+
+		if ($active_tab == 'style_options') {
+
+			?>
+
+			<div class="yasr-settingsdiv">
+			        <form action="options.php" method="post" id="yasr_settings_form">
+			            <?php
+				            settings_fields( 'yasr_style_options_group' );
+				            do_settings_sections('yasr_style_tab' );
+			            	submit_button( __('Save') );
+			           	?>
+			       	</form>
+			</div>
+
+
+			<?php yasr_donate_dx(); ?>
+
+			<div class="yasr-space-settings-div">
+			</div>
+
+
+			<?php
+
+		} //End tab style
+
+
+		if ($active_tab == 'go_pro') {
+
+             yasr_go_pro();
+
+		}
+
+		?>
+
+		<?php yasr_donate_bottom (); ?>
+
+	<!--End div wrap-->
+	</div> 
+
+
+
+	<script type="text/javascript">
+
+	    jQuery( document ).ready(function() {
+
+	    	var activeTab = <?php echo (json_encode("$active_tab")); ?>;
+
+   			var nMultiSet = <?php echo (json_encode("$n_multi_set")); ?> ;//Null in php is different from javascript NULL
+
+   			var autoInsertEnabled = <?php echo (json_encode(YASR_AUTO_INSERT_ENABLED)); ?>;
+
+		   	YasrSettingsPage(activeTab, nMultiSet, autoInsertEnabled);
+
+	    }); //End jquery document ready
+ 		
+	</script>
---
+++ yet-another-stars-rating.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ * Plugin Name:  Yet Another Stars Rating
+ * Plugin URI: http://wordpress.org/plugins/yet-another-stars-rating/
+ * Description: Rating system with rich snippets
+ * Version: 0.6.3
+ * Author: Dario Curvino
+ * Author URI: http://yetanotherstarsrating.com/
+ * License: GPL2
+ */
+
+/*
+
+Copyright 2014 Dario Curvino (email : d.curvino@tiscali.it)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+    
+define('YASR_VERSION_NUM', '0.6.3');
+
+//Plugin absolute path
+define( "YASR_ABSOLUTE_PATH", dirname(__FILE__) );
+
+//Plugin RELATIVE PATH without slashes (just the directory's name)
+define( "YASR_RELATIVE_PATH", dirname( plugin_basename(__FILE__) ) );
+
+//Plugin language directory: here I've to use relative path
+//because load_plugin_textdomain wants relative and not absolute path
+define( "YASR_LANG_DIR", YASR_RELATIVE_PATH . '/languages/' );
+
+//Js directory
+define ("YASR_JS_DIR",  plugins_url( YASR_RELATIVE_PATH . '/js/' ));
+
+//CSS directory
+define ("YASR_CSS_DIR", plugins_url( YASR_RELATIVE_PATH . '/css/' ));
+
+//IMG directory
+define ("YASR_IMG_DIR", plugins_url( YASR_RELATIVE_PATH . '/img/'));
+
+/* Include function file */
+
+require (YASR_ABSOLUTE_PATH . '/lib/yasr-functions.php');
+
+require (YASR_ABSOLUTE_PATH . '/lib/yasr-settings-functions.php');
+
+require (YASR_ABSOLUTE_PATH . '/lib/yasr-db-functions.php');
+
+require (YASR_ABSOLUTE_PATH . '/lib/yasr-ajax-functions.php');
+
+require (YASR_ABSOLUTE_PATH . '/lib/yasr-shortcode-functions.php');
+
+$version_installed = get_option('yasr-version') ;
+
+//If this is a fresh new installation
+
+if (!$version_installed ) {
+
+	yasr_install();
+
+}
+
+global $wpdb;
+
+define ("YASR_VOTES_TABLE", $wpdb->prefix . 'yasr_votes');
+
+define ("YASR_MULTI_SET_NAME_TABLE", $wpdb->prefix . 'yasr_multi_set');
+
+define ("YASR_MULTI_SET_FIELDS_TABLE", $wpdb->prefix . 'yasr_multi_set_fields');
+
+define ("YASR_MULTI_SET_VALUES_TABLE", $wpdb->prefix . 'yasr_multi_values');
+
+define ("YASR_LOG_TABLE", $wpdb->prefix . 'yasr_log');
+
+define ("YASR_LOADER_IMAGE", YASR_IMG_DIR . "/loader.gif");
+
+
+//remove mid november
+if ($version_installed && $version_installed < '0.5.0') {
+
+	$option = get_option( 'yasr_general_options' );
+
+    $option['auto_insert_custom_post_only'] = 'no';
+
+    update_option("yasr_general_options", $option);
+
+}
+
+//remove end november
+if ($version_installed && $version_installed < '0.5.4') {
+
+	$option = get_option( 'yasr_general_options' );
+
+    $option['metabox_overall_rating'] = 'stars';
+
+    update_option("yasr_general_options", $option);
+
+}
+
+//remove end gen 2015
+if ($version_installed && $version_installed < '0.5.9') {
+
+	$wpdb->query("ALTER TABLE " . YASR_VOTES_TABLE . " ADD review_type VARCHAR( 10 )");
+
+}
+
+
+
+if ($version_installed != YASR_VERSION_NUM) {
+
+    update_option('yasr-version', YASR_VERSION_NUM);
+
+}
+
+$stored_options = get_option( 'yasr_general_options' );
+
+define ("YASR_AUTO_INSERT_ENABLED", $stored_options['auto_insert_enabled']);
+
+if ( YASR_AUTO_INSERT_ENABLED == 1 ) {
+
+	define ("YASR_AUTO_INSERT_WHAT", $stored_options['auto_insert_what']);
+	define ("YASR_AUTO_INSERT_WHERE", $stored_options['auto_insert_where']);
+	define ("YASR_AUTO_INSERT_SIZE", $stored_options['auto_insert_size']);
+	define ("YASR_AUTO_INSERT_EXCLUDE_PAGES", $stored_options['auto_insert_exclude_pages']);
+
+
+	$custom_post_types = yasr_get_custom_post_type('bool');
+
+	if ($custom_post_types) {
+		define ("YASR_AUTO_INSERT_CUSTOM_POST_ONLY", $stored_options['auto_insert_custom_post_only']);
+	}
+
+	else {
+		define ("YASR_AUTO_INSERT_CUSTOM_POST_ONLY", FALSE);
+	}
+
+}
+
+//Avoid undefined index
+else {
+	define ("YASR_AUTO_INSERT_WHAT", NULL);
+	define ("YASR_AUTO_INSERT_WHERE", NULL);
+	define ("YASR_AUTO_INSERT_SIZE", NULL);
+	define ("YASR_AUTO_INSERT_EXCLUDE_PAGES", NULL);
+	define ("YASR_AUTO_INSERT_CUSTOM_POST_ONLY", NULL);
+
+}
+
+define ("YASR_SHOW_OVERALL_IN_LOOP", $stored_options['show_overall_in_loop']);
+define ("YASR_TEXT_BEFORE_STARS", $stored_options['text_before_stars']);
+
+if ( YASR_TEXT_BEFORE_STARS == 1 ) {
+
+	define ("YASR_TEXT_BEFORE_OVERALL", $stored_options['text_before_overall']);
+	define ("YASR_TEXT_BEFORE_VISITOR_RATING", $stored_options['text_before_visitor_rating']);
+	define ("YASR_CUSTOM_TEXT_USER_VOTED", $stored_options['custom_text_user_voted']);
+
+}
+
+define ("YASR_SNIPPET", $stored_options['snippet']);
+define ("YASR_ALLOWED_USER", $stored_options['allowed_user']);
+define ("YASR_SCHEME_COLOR", $stored_options['scheme_color']);
+
+//Get stored style options 
+$custom_style = get_option ('yasr_style_options');
+
+if ($custom_style && $custom_style['textarea'] != '') {
+
+	define ("YASR_CUSTOM_CSS_RULES", $custom_style['textarea']);
+
+}
+
+else {
+
+	define ("YASR_CUSTOM_CSS_RULES", NULL);
+
+}
+
+define ("YASR_METABOX_OVERALL_RATING", $stored_options['metabox_overall_rating']);    
+
+
+?>
